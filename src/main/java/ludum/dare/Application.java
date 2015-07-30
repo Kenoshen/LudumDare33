3d502eba85b46c95acffd1e88285f8af81b9e78f
Michael Wingfield <michael.wingfield@twcable.com>
2015-07-29 23:01:15 -0600

    Just did another little test, we will see if it works

--- src/main/java/ludum/dare/Application.java
+++ src/main/java/ludum/dare/Application.java
@@ -6,6 +6,7 @@ import com.winger.utils.GlobalClipboard;
 
 public class Application
 {
+    // another test comment
     public static void main(String[] arg)
     {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
