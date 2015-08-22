a981f3ebf66905fe713e801a030c8700b49bf4b4
Tanner Moore <tanner.moore@logrhythm.com>
2015-08-22 12:49:29 -0600

    Art. Animation tweaking.

--- src/main/java/ludum/dare/trait/DrawableTrait.java
+++ src/main/java/ludum/dare/trait/DrawableTrait.java
@@ -2,27 +2,40 @@ package ludum.dare.trait;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by mwingfield on 8/2/15.
  */
 public class DrawableTrait extends Trait {
-    private static Class[] REQUIRES = new Class[]{ PositionTrait.class, SizeTrait.class };
+    private static Class[] REQUIRES = new Class[]{ PositionTrait.class };
 
     public Sprite sprite;
     private PositionTrait pos;
-    private SizeTrait size;
+    public Vector2 offset;
+    private boolean fixedSize = false;
+
+    public DrawableTrait(GameObject obj) {
+        this(obj, null);
+    }
 
     public DrawableTrait(GameObject obj, Sprite sprite) {
         super(obj);
-        this.sprite = sprite;
+        if (sprite == null) {
+            fixedSize = false;
+            this.sprite = new Sprite();
+        } else {
+            fixedSize = true;
+            this.sprite = sprite;
+            updateSpriteSize();
+        }
     }
 
     @Override
     public void initialize(){
         super.initialize();
         pos = self.getTrait(PositionTrait.class);
-        size = self.getTrait(SizeTrait.class);
+        offset = new Vector2(0,0);
     }
 
     @Override
@@ -33,10 +46,12 @@ public class DrawableTrait extends Trait {
     public void draw(SpriteBatch spriteBatch){
         if (sprite != null) {
             // TODO: not sure why the size has to be double... that is really confusing me...
-            sprite.setSize(size.width * 2, size.height * 2);
+            if (!fixedSize) {
+                updateSpriteSize();
+            }
             sprite.setOrigin(sprite.getWidth() / 2f, sprite.getHeight() / 2f);
-            sprite.setX(pos.x - sprite.getWidth() / 2f); // also, this stuff seems kind of hacky?
-            sprite.setY(pos.y - sprite.getHeight() / 2f);
+            sprite.setX(pos.x - sprite.getWidth() / 2f + offset.x); // also, this stuff seems kind of hacky?
+            sprite.setY(pos.y - sprite.getHeight() / 2f + offset.y);
 
             sprite.setRotation(pos.rotation);
 
@@ -44,5 +59,9 @@ public class DrawableTrait extends Trait {
                 sprite.draw(spriteBatch);
             }
         }
+    }
+
+    private void updateSpriteSize() {
+        sprite.setSize(sprite.getWidth() * 2, sprite.getHeight() * 2);
     }
 }
--- src/main/java/ludum/dare/utils/NamedAnimation.java
+++ src/main/java/ludum/dare/utils/NamedAnimation.java
@@ -3,38 +3,31 @@ package ludum.dare.utils;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by Admin on 8/21/2015.
  */
 public class NamedAnimation extends Animation{
+    private static float scaleFactor = .5f;
     private String name;
+    private Vector2 offset;
     /**
      * Holds the last frame we last returned
      */
     private int lastCalledFrame;
+    private Vector2 size = new Vector2();
 
     public NamedAnimation(String name, float frameDuration, Array<? extends TextureRegion> keyFrames) {
-        super(frameDuration, keyFrames);
-        this.name = name;
+        this(name, frameDuration, keyFrames, new Vector2(0, 0), new Vector2(1, 1));
     }
 
-    public NamedAnimation(String name, float frameDuration, Array<? extends TextureRegion> keyFrames, PlayMode playMode) {
-        super(frameDuration, keyFrames, playMode);
-        this.name = name;
-    }
-
-    public NamedAnimation(String name, float frameDuration, TextureRegion... keyFrames) {
+    public NamedAnimation(String name, float frameDuration, Array<? extends TextureRegion> keyFrames, Vector2 offset, Vector2 size) {
         super(frameDuration, keyFrames);
-        this.setPlayMode(PlayMode.LOOP);
         this.name = name;
-    }
-
-    public NamedAnimation(String name, float frameDuration, TextureAtlas.AtlasRegion[] keyFrames) {
-        super(frameDuration, keyFrames);
-        this.setPlayMode(PlayMode.LOOP);
-        this.name = name;
+        this.offset = offset;
+        this.size = size;
     }
 
     @Override
@@ -45,6 +38,7 @@ public class NamedAnimation extends Animation{
 
     @Override
     public TextureRegion getKeyFrame(float stateTime, boolean looping) {
+        setPlayMode(looping ? PlayMode.LOOP : PlayMode.NORMAL);
         lastCalledFrame = super.getKeyFrameIndex(stateTime);
         return super.getKeyFrame(stateTime, looping);
     }
@@ -55,5 +49,13 @@ public class NamedAnimation extends Animation{
 
     public int getLastCalledFrame() {
         return lastCalledFrame;
+    }
+
+    public Vector2 getOffset() {
+        return offset;
+    }
+
+    public Vector2 getSize() {
+        return size;
     }
 }
