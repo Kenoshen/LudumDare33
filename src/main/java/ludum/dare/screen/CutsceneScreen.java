bc125ad384e5b36dfe6afd62953c0496cf71c92a
Tanner Moore <tanner.moore@logrhythm.com>
2015-08-22 00:39:22 -0600

    Cutscene screen beginning

--- src/main/java/ludum/dare/screen/CutsceneScreen.java
+++ src/main/java/ludum/dare/screen/CutsceneScreen.java
@@ -2,14 +2,20 @@ package ludum.dare.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
+import com.badlogic.gdx.utils.Align;
 import ludum.dare.Game;
+import ludum.dare.utils.SkinManager;
+
+import java.util.ArrayList;
 
 
 public class CutsceneScreen implements Screen {
@@ -17,40 +23,81 @@ public class CutsceneScreen implements Screen {
     private Game game;
     private Stage stage = new Stage();
 
-    private TextField storyTextField;
-    BitmapFont whiteText;
+    ArrayList<Label> listStoryText;
+    BitmapFont bitmapFont;
 
     public CutsceneScreen(final Game game){
         this.game = game;
 
+        listStoryText = new ArrayList<>();
 
+        listStoryText.add(generateWrappableLabel("In the future... ", SkinManager.instance.getSkin("menu-skin"), true));
+        listStoryText.add(generateWrappableLabel("Hobos are considered a myth... ", SkinManager.instance.getSkin("menu-skin"), true));
+        listStoryText.add(generateWrappableLabel("These \"creatures\" are found \nonly in children's tales... ", SkinManager.instance.getSkin("menu-skin"), true));
+        listStoryText.add(generateWrappableLabel("Depicted as killers and robbers... ", SkinManager.instance.getSkin("menu-skin"), true));
+        listStoryText.add(generateWrappableLabel("How would people react if one showed up? ", SkinManager.instance.getSkin("menu-skin"), true));
 
-//        TextFieldStyle(BitmapFont font, Color fontColor, Drawable cursor, Drawable selection, Drawable background)
+        bitmapFont = new BitmapFont();
 
-//        storyTextField = new TextField("Hello", skin);
+        for(Label l : listStoryText) {
+            Container tmpContainer = new Container(l);
+            tmpContainer.setFillParent(true);
+            l.setAlignment(Align.center);
+            stage.addActor(tmpContainer);
+        }
 
         Gdx.input.setInputProcessor(stage);
     }
 
+    private Label generateWrappableLabel(String text, Skin skin, boolean wrappable){
+        Label tmp = new Label(text, skin);
+        tmp.setFontScale(.85f);
+        tmp.setWrap(wrappable);
+
+        return tmp;
+    }
+
     @Override
     public void show() {
-        storyTextField.addAction(
-                Actions.sequence(
-                        Actions.alpha(0),
-                        Actions.delay(0.25f),
-                        Actions.fadeIn(0.5f),
-                        Actions.delay(1.5f),
-                        Actions.fadeOut(0.5f),
-                        Actions.run(
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
 
-                                    }
-                                }
-                        )
-                )
-        );
+        for(Label l : listStoryText){
+            l.addAction(Actions.alpha(0));
+        }
+
+        showNextText(0);
+    }
+
+    private void showNextText(final int index){
+
+        if(index == listStoryText.size()){
+            return;
+        }
+
+        if(index == listStoryText.size()-1){
+            listStoryText.get(index).addAction(Actions.sequence(
+                    Actions.delay(0.25f),
+                    Actions.fadeIn(0.5f),
+                    Actions.delay(3.5f),
+                    Actions.fadeOut(0.5f),
+                    Actions.run(new Runnable() {
+                        @Override
+                        public void run() {
+                            game.setScreen(new SplashScreen(game));
+                        }
+                    })));
+        }
+
+        listStoryText.get(index).addAction(Actions.sequence(
+                Actions.delay(0.25f),
+                Actions.fadeIn(0.5f),
+                Actions.delay(3.5f),
+                Actions.fadeOut(0.5f),
+                Actions.run(new Runnable() {
+                    @Override
+                    public void run() {
+                        showNextText(index + 1);
+                    }
+                })));
     }
 
     @Override
