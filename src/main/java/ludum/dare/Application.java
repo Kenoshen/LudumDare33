8ff9b4f0a14e68898f0de59fd44345246e064347
Michael Wingfield <mwingfie@gmail.com>
2015-08-22 02:33:17 -0600

    added icon files and integrated into application configuration

--- src/main/java/ludum/dare/Application.java
+++ src/main/java/ludum/dare/Application.java
@@ -1,5 +1,6 @@
 package ludum.dare;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.winger.log.HTMLLogger;
@@ -14,6 +15,9 @@ public class Application
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 800;
         config.height = 450;
+        config.addIcon("imgs/icons/icon_128.png", Files.FileType.Internal);
+        config.addIcon("imgs/icons/icon_32.png", Files.FileType.Internal);
+        config.addIcon("imgs/icons/icon_16.png", Files.FileType.Internal);
         System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
         Game game = new Game();
         com.badlogic.gdx.Application app = new LwjglApplication(game, config);
