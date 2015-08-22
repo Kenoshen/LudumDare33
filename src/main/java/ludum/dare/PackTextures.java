b4b70a113ff54675fbe98355ec52c172ca261c1b
Tanner Moore <tanner.moore@logrhythm.com>
2015-08-22 10:56:42 -0600

    Add settings to texture packing

--- src/main/java/ludum/dare/PackTextures.java
+++ src/main/java/ludum/dare/PackTextures.java
@@ -1,5 +1,6 @@
 package ludum.dare;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class PackTextures
@@ -7,9 +8,12 @@ public class PackTextures
     public static void main(String[] arg)
     {
         System.out.println("Start packing textures");
-        TexturePacker.process("src/main/resources/imgs/ui/menu", "src/main/resources/packed", "ui");
-        TexturePacker.process("src/main/resources/imgs/ui/background", "src/main/resources/packed", "ui-background");
-        TexturePacker.process("src/main/resources/imgs/game", "src/main/resources/packed", "game");
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.filterMin = Texture.TextureFilter.Linear;
+        settings.filterMag = Texture.TextureFilter.Linear;
+        TexturePacker.process(settings, "src/main/resources/imgs/ui/menu", "src/main/resources/packed", "ui");
+        TexturePacker.process(settings, "src/main/resources/imgs/ui/background", "src/main/resources/packed", "ui-background");
+        TexturePacker.process(settings, "src/main/resources/imgs/game", "src/main/resources/packed", "game");
         System.out.println("Finished packing textures");
     }
 }
