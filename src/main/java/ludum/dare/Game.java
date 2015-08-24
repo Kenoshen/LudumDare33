bb6025c4d575496451b991934a3e381022ee4351
Michael Wingfield <mwingfie@gmail.com>
2015-08-23 19:55:07 -0600

    fixed sub level thing with static ref to game screen

--- src/main/java/ludum/dare/Game.java
+++ src/main/java/ludum/dare/Game.java
@@ -66,10 +66,7 @@ public class Game extends com.badlogic.gdx.Game {
         //setScreen(new CutsceneScreen(this));
         //setScreen(new GameScreen(this, new TestLevel3()));
         //setScreen(new MainMenuScreen(this));
-        TestSubLevels level = new TestSubLevels();
-        GameScreen screen = new GameScreen(this, level);
-        level.gameScreen = screen;
-        setScreen(screen);
+        setScreen(new GameScreen(this, new TestSubLevels()));
     }
 
 
